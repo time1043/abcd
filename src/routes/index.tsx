fbab5ad601a3d4d8bb0e59a6929e4fb78eb33389
oswin902m <1665434994@qq.com>
2026-05-22 17:47:19 +0800

    chore(init): setup shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import { Button } from '@/components/ui/button';
+
 export const Route = createFileRoute('/')({
   component: Index,
 });
@@ -8,6 +10,7 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button>Click Me</Button>
     </div>
   );
 }
