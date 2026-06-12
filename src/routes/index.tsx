c4ac955f905f963474d855c8d232d8b4a34f713f
oswin902m <1665434994@qq.com>
2026-06-12 14:46:22 +0800

    chore(fmt): format with oxfmt

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
-export const Route = createFileRoute('/')({ component: Home })
+export const Route = createFileRoute('/')({
+  component: RouteComponent,
+});
 
-function Home() {
+function RouteComponent() {
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
@@ -10,5 +12,5 @@ function Home() {
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
     </div>
-  )
+  );
 }
